9f6d9b88ab3b4dd84ed1d9bd29534395598fce81
David Kleuker <david.kleuker@greenbone.net>
2018-10-17 18:12:40 +0200

    Add logging, error handling and license

--- moinmoin_import.py
+++ moinmoin_import.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2018 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" Import file content into MoinMoin Wiki
+
+The file name (without extension) get automatically appended to the base URL.
+
+For example with parameters --file devcons.md and
+--url https://intra.greenbone.net/QM/test/ the path of the new page is
+https://intra.greenbone.net/QM/test/devcons.
+
+Existing content will be overwritten!
+But MoinMoin is versioned, so nothing is lost.
+"""
 
 import os
+import sys
 import argparse
 import logging
 import requests
@@ -9,22 +39,32 @@ import bs4
 from urllib.parse import urljoin
 
 
-def login(username, password):
+def login(url, username, password):
     """Login to MoinMoin Wiki and returns session cookie"""
     payload = {'action':'login', 'name': username, 'password': password,
                'login': 'Login', 'login': 'Login'}
-    r = requests.post('https://intra.greenbone.net/', data = payload)
-    return r.cookies
+    r = requests.post(url, data = payload)
+    if r.status_code == 200:
+        logging.info('Successfully logged in as {}'.format(username))
+        return r.cookies
+    else:
+        r.raise_for_status()
 
 
 def get_ticket(url, session):
     """Get ticket and new rev for URL"""
     payload = {'action': 'edit', 'editor': 'text'}
     r = requests.get(url, params = payload, cookies = session)
-    html = bs4.BeautifulSoup(r.text, features='html.parser')
-    ticket = html.find(attrs={"name": "ticket"})['value']
-    rev = html.find(attrs={"name": "rev"})['value']
-    return ticket, rev
+    r.raise_for_status()
+    try:
+        html = bs4.BeautifulSoup(r.text, features='html.parser')
+        ticket = html.find(attrs={"name": "ticket"})['value']
+        rev = html.find(attrs={"name": "rev"})['value']
+        logging.info('Got ticket to edit {}'.format(url))
+        return ticket, rev
+    except:
+        logging.critical('Failed to get ticket to edit {}'.format(url))
+        sys.exit(1)
 
 
 def edit_page(url, session, text, ticket, rev):
@@ -33,6 +73,8 @@ def edit_page(url, session, text, ticket, rev):
                'ticket': ticket, 'button_save': 'Save Changes',
                'savetext': text, 'comment': 'Automated import'}
     r = requests.post(url, data = payload, cookies = session)
+    r.raise_for_status()
+    logging.info('Successfully edited page {}'.format(url))
 
 
 def main():
@@ -46,9 +88,18 @@ def main():
     parser.add_argument('-b', '--url', required = True,
                         help = 'Base URL for page '
                                 'like https://intra.greenbone.net/QM/test/')
+    parser.add_argument('-l', '--log', dest='loglevel', default='INFO',
+                        choices=['DEBUG', 'INFO', 'WARNING',
+                                 'ERROR', 'CRITICAL'],
+                        help='Log level. Default: INFO')
     args = parser.parse_args()
 
-    session = login(args.username, args.password)
+    LEVEL = logging.getLevelName(args.loglevel)
+    FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level = LEVEL, format = FORMAT,
+                        datefmt = '%Y-%m-%d %H:%M:%S')
+
+    session = login(args.url, args.username, args.password)
     file_name = os.path.splitext(os.path.basename(args.file))[0]
     url = urljoin(args.url, file_name)
     ticket, rev = get_ticket(url, session)
